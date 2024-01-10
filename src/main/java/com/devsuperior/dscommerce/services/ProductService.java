58996be5d61360308c42ac80832b5ca8603c1ef1
nexola <vitormatheusfv@gmail.com>
2024-01-10 13:21:25 -0300

    DELETE Product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -47,5 +47,9 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
 
