317d72cf478b1fc99c6056cc506b24775d3fd1fd
nexola <vitormatheusfv@gmail.com>
2024-02-05 18:58:37 -0300

    Teste unitário OrderService insert

--- src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
@@ -2,12 +2,17 @@ package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
 import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.factories.OrderFactory;
+import com.devsuperior.dscommerce.factories.ProductFactory;
 import com.devsuperior.dscommerce.factories.UserFactory;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
 import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ForbbidenException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,8 +21,10 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
@@ -32,10 +39,21 @@ public class OrderServiceTests {
     @Mock
     private AuthService authService;
 
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private OrderItemRepository orderItemRepository;
+
+    @Mock
+    private UserService userService;
+
     private Long existingOrderId, nonExistingOrderId;
     private Order order;
     private OrderDTO orderDTO;
     private User admin, client;
+    private Long existingProductId, nonExistingProductId;
+    private Product product;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -45,9 +63,19 @@ public class OrderServiceTests {
         client = UserFactory.createCustomClientUser(2L, "Bob");
         order = OrderFactory.createOrder(client);
         orderDTO = new OrderDTO(order);
+        existingProductId = 1L;
+        nonExistingProductId = 2L;
+        product = ProductFactory.createProduct();
 
         Mockito.when(orderRepository.findById(existingOrderId)).thenReturn(Optional.of(order));
         Mockito.when(orderRepository.findById(nonExistingOrderId)).thenReturn(Optional.empty());
+
+        Mockito.when(productRepository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
+        Mockito.when(productRepository.getReferenceById(existingProductId)).thenReturn(product);
+
+        Mockito.when(orderRepository.save(ArgumentMatchers.any())).thenReturn(order);
+
+        Mockito.when(orderItemRepository.saveAll(ArgumentMatchers.any())).thenReturn(new ArrayList<>(order.getItems()));
 
     }
 
@@ -75,6 +103,27 @@ public class OrderServiceTests {
 
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             orderService.findById(nonExistingOrderId);
+        });
+    }
+
+    @Test
+    public void insertShouldReturnOrderDTOWhenAdminOrClientLogged() {
+        Mockito.when(userService.authenticated()).thenReturn(client);
+        OrderDTO result = orderService.insert(orderDTO);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getId(), existingOrderId);
+    }
+
+    @Test
+    public void insertShouldThrowsUsernameNotFoundExceptionWhenUserNotLogged() {
+        Mockito.doThrow(UsernameNotFoundException.class).when(userService).authenticated();
+
+        order.setClient(new User());
+        orderDTO = new OrderDTO(order);
+
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            orderService.insert(orderDTO);
         });
     }
 }
